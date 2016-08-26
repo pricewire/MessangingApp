5ad0006b7805fc579b654865b88fc0d0050e570e
Mark Langlois <pricewire@comcast.net>
2016-08-26 16:52:51 -0400

    My 2nd Commit

--- MessangerApp/AppDelegate.swift
+++ MessangerApp/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        FIRApp.configure()
+        
+        
         // Override point for customization after application launch.
         return true
     }
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+
+target 'MessangerApp' do
+
+pod ‘Firebase’
+pod ‘Firebase/Auth’
+pod ‘Firebase/Storage’
+pod ‘Firebase/Database’
+
+
+
+
+
+end
